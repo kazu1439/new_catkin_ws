b3af046611fe351a1e82a42cd6f28234beacc157
kazu1439 <titanda7@gmail.com>
2022-02-05 18:17:11 +0900

    関数解説用プログラム作成

---
+++ src/beginner_tutorials/src/kansu.cpp
@@ -0,0 +1,65 @@
+#include "ros/ros.h"
+
+#define CTRL_PERIOD 0.02f
+
+int tasu(int a, int b);
+
+int hiku(int something, int anything);
+
+int kakeru(int someone, int anyone);
+
+float waru(float somewhere, float anywhere);
+
+void keisan(int num1, int num2);
+
+
+
+int main(int argc, char **argv){
+    
+    ros::init( argc, argv, "kansu" );
+    ros::NodeHandle nh;
+    ros::Rate loop_rate( 1.0f/CTRL_PERIOD );
+    while ( ros::ok() ){
+        
+        //tasu()
+        int ans = tasu(1,2);
+        ROS_INFO("tasu->%d\n",ans);
+        
+        //hiku()
+        ROS_INFO("hiku->%d\n",hiku(9,3));
+        
+        //kakeru() & waru()
+        ROS_INFO("kakeru & waru->%f\n",waru(12,kakeru(2,3)));
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}
+
+int tasu(int a, int b){//足し算
+    int c;
+    c = a + b;
+    return c;
+}
+
+int hiku(int something, int anything){//引き算
+    return something - anything;
+}
+
+int kakeru(int someone, int anyone){//かけ算
+    int they = someone * anyone;
+    return they;
+}
+
+float waru(float somewhere, float anywhere){//割り算
+    float house = somewhere / anywhere;
+    return house;
+}
+
+void keisan(int num1, int num2){//全部
+    ROS_INFO("%d",tasu(num1,num2));
+    ROS_INFO("%d",hiku(num1,num2));
+    ROS_INFO("%d",kakeru(num1,num2));
+    ROS_INFO("%f\n",waru(num1,num2));
+}
+
